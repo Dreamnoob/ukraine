7fc6ac5e9609fcd9b9a7065a1c197f4161620228
dreamnoob <vitaliklio1100@gmail.com>
2022-05-01 13:45:07 +0300

    add new changes

--- src/js/main.js
+++ src/js/main.js
@@ -44,5 +44,29 @@ if (map) {
     });
 }
 
+let viewport = document.querySelector(".svg-pan-zoom_viewport");
 
+let text = `
+    <svg class="empty__svg"  width="74" height="42" viewBox="0 0 74 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_d_4_9071)">
+    </g>
+    <defs>
+    <filter id="filter0_d_4_9071" x="0.945435" y="0.0412598" width="72.4776" height="41.9098" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+    <feOffset dy="1"/>
+    <feComposite in2="hardAlpha" operator="out"/>
+    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.5 0"/>
+    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_4_9071"/>
+    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_4_9071" result="shape"/>
+    </filter>
+    <linearGradient id="paint0_linear_4_9071" x1="53%" y1="0%" x2="50%" y2="100%" gradientUnits="objectBoundingBox">
+    <stop offset="0.392336" stop-color="#005BBB"/>
+    <stop offset="0.408687" stop-color="#FFD500"/>
+    </linearGradient>
+    </defs>
+    </svg>
+`;
+
+$(text).appendTo(viewport);
 
